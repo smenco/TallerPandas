3217f8c572a9d6325cd4c16c7f00115cf9d4a012
Sergio Rafael Menco Ariza <smenco@serfinansa.local>
2022-05-27 16:03:44 -0500

    segundo commit

--- Taller Covid Colombia.py
+++ Taller Covid Colombia.py
@@ -20,7 +20,7 @@ data.drop('Código DIVIPOLA departamento', axis = 1, inplace=True)
 data.drop('Código DIVIPOLA municipio', axis = 1, inplace=True)
 data.drop('ID de caso', axis = 1, inplace=True)
 
-data['Tipo de recuperación'].value_counts()
+data['Recuperado'].value_counts()
 
 data.loc[data['Estado'] == 'leve'] = 'Leve'
 data.loc[data['Estado'] == 'LEVE'] = 'Leve'
@@ -52,8 +52,8 @@ aux = data.loc[(data['Estado'] == 'Fallecido')]
 NumeroDePersonasFallecidas = aux.shape[0]
 
 #Ordenar de Mayor a menor por tipo de caso (Importado, en estudio,Relacionado)
-data.sort_values(by='Tipo de contagio',ascending=False )
-
+data.sort_values(by=data.loc[(data['Tipo de contagio'] == 'Importado')],ascending=False )
+data.sort_values(by=data.loc[(data['Tipo de contagio'] == 'Relacionado')],ascending=False )
 #Número de departamentos afectados
 data['Nombre departamento'].nunique()
 
@@ -62,3 +62,36 @@ data['Nombre departamento'].value_counts()
 
 #Ordene de mayor a menor por tipo de atención
 data.sort_values(by='Tipo de recuperación',ascending=False )
+
+#Liste de mayor a menor los 10 departamentos con mas casos de contagiados
+data['Nombre departamento'].value_counts().head(10)
+
+#Liste de mayor a menor los 10 departamentos con mas casos de fallecidos
+aux = data[(data['Estado'] == 'Fallecido')].groupby('Nombre departamento').size()
+
+aux.sort_values(ascending=False).head(10)
+
+#Liste de mayor a menor los 10 departamentos con mas casos de recuperados
+aux = data[(data['Recuperado'] == 'Recuperado')].groupby('Nombre departamento').size()
+
+aux.sort_values(ascending=False).head(10)
+
+#Liste de mayor a menor los 10 municipios con mas casos de contagiados
+data['Nombre municipio'].value_counts().head(10)
+
+#Liste de mayor a menor los 10 municipios con mas casos de fallecidos
+aux = data[(data['Estado'] == 'Fallecido')].groupby('Nombre municipio').size()
+
+aux.sort_values(ascending=False).head(10)
+
+#Liste de mayor a menor los 10 municipios con mas casos de recuperados
+aux = data[(data['Recuperado'] == 'Recuperado')].groupby('Nombre municipio').size()
+
+aux.sort_values(ascending=False).head(10)
+
+
+
+
+
+
+
